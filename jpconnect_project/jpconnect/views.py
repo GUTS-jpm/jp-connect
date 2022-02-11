97e1857335c11edccd81b0202f60e10f48fbf82c
swpinkerton <swpinkerton11@gmail.com>
2022-02-11 21:08:03 +0000

    scott tempcommit

--- jpconnect_project/jpconnect/views.py
+++ jpconnect_project/jpconnect/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+import http.client, urllib.parse
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def news(request):
+    conn = http.client.HTTPSConnection('api.thenewsapi.com')
+
+    params = urllib.parse.urlencode({
+        'api_token': 'AYtAStyVMqv1p6H2y7OpXrQ0ITWs0KUw0ZPtzdQG',
+        # 'categories': 'business,tech',
+        'limit': 5,
+        'search' : 'london',
+        })
+
+    conn.request('GET', '/v1/news/all?{}'.format(params))
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode('utf-8'))
     return render(request, 'news.html')
